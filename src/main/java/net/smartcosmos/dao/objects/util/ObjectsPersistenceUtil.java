44311edfec87f03e166f055fda972357ced08c05
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-05-20 14:44:21 +0200

    Let the validation on saving do the validation

--- src/main/java/net/smartcosmos/dao/objects/util/ObjectsPersistenceUtil.java
+++ src/main/java/net/smartcosmos/dao/objects/util/ObjectsPersistenceUtil.java
@@ -2,17 +2,16 @@ package net.smartcosmos.dao.objects.util;
 
 import net.smartcosmos.dao.objects.domain.ObjectEntity;
 import net.smartcosmos.dto.objects.ObjectUpdate;
-import org.springframework.util.StringUtils;
 
 public class ObjectsPersistenceUtil {
 
     public static ObjectEntity merge(ObjectEntity objectEntity, ObjectUpdate updateObject) {
 
-        if (!StringUtils.isEmpty(updateObject.getName())) {
+        if (updateObject.getName() != null) {
             objectEntity.setName(updateObject.getName());
         }
 
-        if (!StringUtils.isEmpty(updateObject.getType())) {
+        if (updateObject.getType() != null) {
             objectEntity.setType(updateObject.getType());
         }
 
