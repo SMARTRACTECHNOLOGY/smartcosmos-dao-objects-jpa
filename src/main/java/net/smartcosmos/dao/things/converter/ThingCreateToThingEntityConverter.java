1b7ccb9dc4be5bb5283e81a39e02a31a8f636f76
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-15 14:58:08 +0200

    Remove user from converter

--- src/main/java/net/smartcosmos/dao/things/converter/ThingCreateToThingEntityConverter.java
+++ src/main/java/net/smartcosmos/dao/things/converter/ThingCreateToThingEntityConverter.java
@@ -1,33 +1,24 @@
 package net.smartcosmos.dao.things.converter;
 
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistrar;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Component;
 
 import net.smartcosmos.dao.things.domain.ThingEntity;
 import net.smartcosmos.dto.things.ThingCreate;
-import net.smartcosmos.security.user.SmartCosmosUser;
-import net.smartcosmos.security.user.SmartCosmosUserHolder;
-import net.smartcosmos.util.UuidUtil;
 
 @Component
 public class ThingCreateToThingEntityConverter
         implements Converter<ThingCreate, ThingEntity>, FormatterRegistrar {
 
     @Override
-    public ThingEntity convert(ThingCreate objectCreate) {
-
-        // Retrieve current user.
-        SmartCosmosUser user = SmartCosmosUserHolder.getCurrentUser();
+    public ThingEntity convert(ThingCreate thingCreate) {
 
         return ThingEntity.builder()
-                // Required
-                .urn(objectCreate.getUrn()).type(objectCreate.getType())
-                .tenantId(UuidUtil.getUuidFromAccountUrn(user.getAccountUrn()))
-                // Optional
-                .active(objectCreate.getActive())
-                .build();
+            .urn(thingCreate.getUrn())
+            .type(thingCreate.getType())
+            .active(thingCreate.getActive())
+            .build();
     }
 
     @Override
