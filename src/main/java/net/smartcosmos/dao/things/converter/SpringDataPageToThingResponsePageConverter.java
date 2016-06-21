675da288e1da5da9b565840b5f43ff1688b45879
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-21 10:49:00 +0200

    Add converter for pages

---
+++ src/main/java/net/smartcosmos/dao/things/converter/SpringDataPageToThingResponsePageConverter.java
@@ -0,0 +1,54 @@
+package net.smartcosmos.dao.things.converter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.FormatterRegistrar;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.stereotype.Component;
+
+import net.smartcosmos.dao.things.domain.ThingEntity;
+import net.smartcosmos.dto.things.Page;
+import net.smartcosmos.dto.things.PageInformation;
+import net.smartcosmos.dto.things.ThingResponse;
+
+@Component
+public class SpringDataPageToThingResponsePageConverter
+    extends ConversionServiceAwareConverter<org.springframework.data.domain.Page<ThingEntity>, Page<ThingResponse>>
+    implements FormatterRegistrar {
+
+    @Inject
+    private ConversionService conversionService;
+
+    protected ConversionService conversionService() {
+        return conversionService;
+    }
+
+    @Override
+    public Page<ThingResponse> convert(org.springframework.data.domain.Page<ThingEntity> page) {
+
+        PageInformation pageInformation = PageInformation.builder()
+            .number(page.getNumber())
+            .totalElements(page.getTotalElements())
+            .size(page.getNumberOfElements())
+            .totalPages(page.getTotalPages())
+            .build();
+
+        List<ThingResponse> data = page.getContent().stream()
+            .map(entity -> conversionService.convert(entity, ThingResponse.class))
+            .collect(Collectors.toList());
+
+        return Page.<ThingResponse>builder()
+            .data(data)
+            .page(pageInformation)
+            .build();
+    }
+
+    @Override
+    public void registerFormatters(FormatterRegistry registry) {
+        registry.addConverter(this);
+    }
+}
