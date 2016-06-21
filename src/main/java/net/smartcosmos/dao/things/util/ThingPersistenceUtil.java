0b071de35fe346ce28bf62fb4d706c1e101edc62
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-21 09:53:00 +0200

    Move emptyPage to Util

--- src/main/java/net/smartcosmos/dao/things/util/ThingPersistenceUtil.java
+++ src/main/java/net/smartcosmos/dao/things/util/ThingPersistenceUtil.java
@@ -1,7 +1,12 @@
 package net.smartcosmos.dao.things.util;
 
 import net.smartcosmos.dao.things.domain.ThingEntity;
+import net.smartcosmos.dto.things.Page;
+import net.smartcosmos.dto.things.PageInformation;
+import net.smartcosmos.dto.things.ThingResponse;
 import net.smartcosmos.dto.things.ThingUpdate;
+
+import java.util.ArrayList;
 
 public class ThingPersistenceUtil {
 
@@ -12,5 +17,10 @@ public class ThingPersistenceUtil {
         }
 
         return thingEntity;
+    }
+
+    public static Page<ThingResponse> emptyPage() {
+
+        return new Page<>(new ArrayList<>(), new PageInformation());
     }
 }
