fe276795a077132bf717bfd8a74a2038684c2514
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-21 09:52:28 +0200

    Add regions and implement new DAO methods

--- src/main/java/net/smartcosmos/dao/things/impl/ThingPersistenceService.java
+++ src/main/java/net/smartcosmos/dao/things/impl/ThingPersistenceService.java
@@ -1,11 +1,14 @@
 package net.smartcosmos.dao.things.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import net.smartcosmos.dao.things.SortOrder;
 import net.smartcosmos.dao.things.ThingDao;
 import net.smartcosmos.dao.things.domain.ThingEntity;
 import net.smartcosmos.dao.things.repository.ThingRepository;
 import net.smartcosmos.dao.things.util.ThingPersistenceUtil;
 import net.smartcosmos.dao.things.util.UuidUtil;
+import net.smartcosmos.dto.things.Page;
+import net.smartcosmos.dto.things.PageInformation;
 import net.smartcosmos.dto.things.ThingCreate;
 import net.smartcosmos.dto.things.ThingResponse;
 import net.smartcosmos.dto.things.ThingUpdate;
@@ -13,6 +16,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionException;
 
@@ -37,6 +41,8 @@ public class ThingPersistenceService implements ThingDao {
         this.repository = repository;
         this.conversionService = conversionService;
     }
+
+    // region Create
 
     @Override
     public Optional<ThingResponse> create(String tenantUrn, ThingCreate createThing) {
@@ -67,10 +73,9 @@ public class ThingPersistenceService implements ThingDao {
         return Optional.empty();
     }
 
-    private boolean alreadyExists(String tenantUrn, ThingCreate createThing) {
+    // endregion
 
-        return StringUtils.isNotBlank(createThing.getUrn()) && findByUrn(tenantUrn, createThing.getUrn()).isPresent();
-    }
+    // region Update
 
     @Override
     public Optional<ThingResponse> update(String tenantUrn, String type, String urn, ThingUpdate updateThing) throws ConstraintViolationException {
@@ -95,8 +100,12 @@ public class ThingPersistenceService implements ThingDao {
         return Optional.empty();
     }
 
+    // endregion
+
+    // region Find By Type
+
     @Override
-    public List<ThingResponse> findByType(String tenantUrn, String type, Long page, Integer size) {
+    public List<ThingResponse> findByType(String tenantUrn, String type) {
 
         try {
             UUID tenantId = UuidUtil.getUuidFromUrn(tenantUrn);
@@ -110,6 +119,28 @@ public class ThingPersistenceService implements ThingDao {
 
         return new ArrayList<>();
     }
+
+    @Override
+    public List<ThingResponse> findByType(String tenantUrn, String type, SortOrder sortOrder, String sortBy) {
+        // TODO: Implement Sorting
+        return findByType(tenantUrn, type);
+    }
+
+    @Override
+    public Page<ThingResponse> findByType(String tenantUrn, String type, Long page, Integer size) {
+        // TODO: Implement Paging
+        return null;
+    }
+
+    @Override
+    public Page<ThingResponse> findByType(String tenantUrn, String type, Integer page, Integer size, SortOrder sortOrder, String sortBy) {
+        // TODO: Implement Paging and Sorting
+        return null;
+    }
+
+    // endregion
+
+    // region Delete
 
     @Override
     public List<ThingResponse> delete(String tenantUrn, String type, String urn) {
@@ -127,6 +158,10 @@ public class ThingPersistenceService implements ThingDao {
 
         return new ArrayList<>();
     }
+
+    // endregion
+
+    // region Find By Type and URN
 
     @Override
     public Optional<ThingResponse> findByTypeAndUrn(String tenantUrn, String type, String urn) {
@@ -146,28 +181,33 @@ public class ThingPersistenceService implements ThingDao {
         return Optional.empty();
     }
 
-    @Override
-    public List<ThingResponse> findByTypeAndUrnStartsWith(String tenantUrn, String type, String urnStartsWith, Long page, Integer size) {
+    // endregion
 
+    // region Find by Type and URN startsWith
+
+    @Override
+    public List<ThingResponse> findByTypeAndUrnStartsWith(String tenantUrn, String type, String urnStartsWith) {
         throw new UnsupportedOperationException("The database implementation does not support 'startsWith' search for URNs");
     }
 
-    private Optional<ThingResponse> findByUrn(String tenantUrn, String urn) {
-
-        try {
-            UUID tenantId = UuidUtil.getUuidFromUrn(tenantUrn);
-            UUID id = UuidUtil.getUuidFromUrn(urn);
-
-            Optional<ThingEntity> entity = repository.findByIdAndTenantId(id, tenantId);
-
-            return convert(entity);
-        }
-        catch (IllegalArgumentException e) {
-            log.warn("Error processing URNs: Tenant URN '{}' - Thing URN '{}'", tenantUrn, urn);
-        }
-
-        return Optional.empty();
+    @Override
+    public List<ThingResponse> findByTypeAndUrnStartsWith(String tenantUrn, String type, String urnStartsWith, SortOrder sortOrder, String sortBy) {
+        throw new UnsupportedOperationException("The database implementation does not support 'startsWith' search for URNs");
     }
+
+    @Override
+    public Page<ThingResponse> findByTypeAndUrnStartsWith(String tenantUrn, String type, String urnStartsWith, Integer page, Integer size) {
+        throw new UnsupportedOperationException("The database implementation does not support 'startsWith' search for URNs");
+    }
+
+    @Override
+    public Page<ThingResponse> findByTypeAndUrnStartsWith(String tenantUrn, String type, String urnStartsWith, Integer page, Integer size, SortOrder sortOrder, String sortBy) {
+        throw new UnsupportedOperationException("The database implementation does not support 'startsWith' search for URNs");
+    }
+
+    // endregion
+
+    // region Find by URNs
 
     @Override
     public List<ThingResponse> findByUrns(String tenantUrn, Collection<String> urns) {
@@ -199,7 +239,17 @@ public class ThingPersistenceService implements ThingDao {
     }
 
     @Override
-    public List<ThingResponse> findAll(String tenantUrn, Long page, Integer size) {
+    public List<ThingResponse> findByUrns(String tenantUrn, Collection<String> urns, SortOrder sortOrder, String sortBy) {
+        // TODO: Implement Sorting
+        return findByUrns(tenantUrn, urns);
+    }
+
+    // endregion
+
+    // region Find All
+
+    @Override
+    public List<ThingResponse> findAll(String tenantUrn) {
 
         try {
             UUID tenantId = UuidUtil.getUuidFromUrn(tenantUrn);
@@ -212,6 +262,34 @@ public class ThingPersistenceService implements ThingDao {
         }
 
         return new ArrayList<>();
+    }
+
+    @Override
+    public List<ThingResponse> findAll(String tenantUrn, SortOrder sortOrder, String sortBy) {
+        // TODO: Implement Sorting
+        return null;
+    }
+
+    @Override
+    public Page<ThingResponse> findAll(String tenantUrn, Integer page, Integer size) {
+
+        try {
+            UUID tenantId = UuidUtil.getUuidFromUrn(tenantUrn);
+            org.springframework.data.domain.Page<ThingEntity> entityList = repository.findByTenantId(tenantId, new PageRequest(page, size));
+
+            return convert(entityList);
+        }
+        catch (IllegalArgumentException e) {
+            log.warn("Error processing URN: Tenant URN '{}'", tenantUrn);
+        }
+
+        return ThingPersistenceUtil.emptyPage();
+    }
+
+    @Override
+    public Page<ThingResponse> findAll(String tenantUrn, Integer page, Integer size, SortOrder sortOrder, String sortBy) {
+        // TODO: Implement Sorting
+        return findAll(tenantUrn, page, size);
     }
 
     /**
@@ -227,6 +305,10 @@ public class ThingPersistenceService implements ThingDao {
         // done inline here.
         return convert(repository.findAll());
     }
+
+    // endregion
+
+    // region Helper Methods
 
     /**
      * Saves an object entity in an {@link ThingRepository}.
@@ -248,6 +330,28 @@ public class ThingPersistenceService implements ThingDao {
                 throw e;
             }
         }
+    }
+
+    private boolean alreadyExists(String tenantUrn, ThingCreate createThing) {
+
+        return StringUtils.isNotBlank(createThing.getUrn()) && findByUrn(tenantUrn, createThing.getUrn()).isPresent();
+    }
+
+    private Optional<ThingResponse> findByUrn(String tenantUrn, String urn) {
+
+        try {
+            UUID tenantId = UuidUtil.getUuidFromUrn(tenantUrn);
+            UUID id = UuidUtil.getUuidFromUrn(urn);
+
+            Optional<ThingEntity> entity = repository.findByIdAndTenantId(id, tenantId);
+
+            return convert(entity);
+        }
+        catch (IllegalArgumentException e) {
+            log.warn("Error processing URNs: Tenant URN '{}' - Thing URN '{}'", tenantUrn, urn);
+        }
+
+        return Optional.empty();
     }
 
     /**
@@ -278,4 +382,19 @@ public class ThingPersistenceService implements ThingDao {
             .map(o -> conversionService.convert(o, ThingResponse.class))
             .collect(Collectors.toList());
     }
+
+    private Page<ThingResponse> convert(org.springframework.data.domain.Page<ThingEntity> page) {
+        List<ThingResponse> data = convert(page.getContent());
+
+        PageInformation pageInformation = PageInformation.builder()
+            .number(page.getNumber())
+            .totalElements(page.getTotalElements())
+            .size(page.getNumberOfElements())
+            .totalPages(page.getTotalPages())
+            .build();
+
+        return new Page<>(data, pageInformation);
+    }
+
+    // endregion
 }
