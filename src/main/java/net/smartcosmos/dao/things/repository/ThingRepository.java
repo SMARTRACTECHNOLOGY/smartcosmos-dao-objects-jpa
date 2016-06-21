6bf498c7c93db1ef13e94e210d7039df228042f1
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-21 14:22:05 +0200

    Add repository methods for paging and sorting

--- src/main/java/net/smartcosmos/dao/things/repository/ThingRepository.java
+++ src/main/java/net/smartcosmos/dao/things/repository/ThingRepository.java
@@ -3,6 +3,7 @@ package net.smartcosmos.dao.things.repository;
 import net.smartcosmos.dao.things.domain.ThingEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -24,9 +25,17 @@ public interface ThingRepository extends JpaRepository<ThingEntity, UUID>, Pagin
 
     List<ThingEntity> findByTenantIdAndType(UUID tenantId, String type);
 
+    List<ThingEntity> findByTenantIdAndType(UUID tenantId, String type, Sort sort);
+
+    Page<ThingEntity> findByTenantIdAndType(UUID tenantId, String type, Pageable pageable);
+
     List<ThingEntity> findByTenantId(UUID tenantId);
 
+    List<ThingEntity> findByTenantId(UUID tenantId, Sort sort);
+
     List<ThingEntity> findByIdInAndTenantId(List<UUID> ids, UUID tenantId);
+
+    List<ThingEntity> findByIdInAndTenantId(List<UUID> ids, UUID tenantId, Sort sort);
 
     Page<ThingEntity> findByTenantId(UUID tenantId, Pageable pageable);
 }
